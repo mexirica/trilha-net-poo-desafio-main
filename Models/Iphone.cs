e1bd29daf95394708ec6d938c61db4e70ec72aad
RodrigoMecheri <rodrigo.mecheri@sitiorecantodoqueijo.com.br>
2023-07-27 15:04:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Iphone.cs
@@ -0,0 +1,16 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nome} no Iphone");
+        }
+    }
+}
---
+++ Models/Nokia.cs
@@ -0,0 +1,17 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nome} no Nokia")
+        }
+    }
+}
